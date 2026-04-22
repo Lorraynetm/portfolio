06b10aab5b29219c0ade613a29af1dc95a49e4a5
Lorrayne Tavares <Lorraynemoreira2003@gmail.com>
2026-04-22 16:10:59 -0300

    fix: ajusta layout e tamanho da imagem no hero

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -5,9 +5,8 @@ function Hero() {
   return (
     <section
       id="home"
-      className="bg-gradient-to-b from-gray-900 to-gray-950 w-full min-h-screen flex items-center"
-    >
-      <div className="max-w-7xl mx-auto w-full grid md:grid-cols-2 items-center gap-12 px-6">
+      className="bg-gradient-to-b from-gray-900 to-gray-950 w-full min-h-screen flex items-center">
+      <div className=" ml-20 mr-auto w-full grid md:grid-cols-[1fr_1.5fr] items-center gap-16 px-6">
         
        
         <div>
@@ -15,7 +14,7 @@ function Hero() {
             Olá, eu sou
           </p>
 
-          <h1 className="pb-4 text-4xl md:text-7xl font-bold text-purple-400">
+          <h1 className="pb-4 text-4xl md:text-7xl font-bold text-purple-400 whitespace-nowrap">
             Lorrayne Tavares
           </h1>
 
@@ -28,8 +27,7 @@ function Hero() {
             speed={50}
             repeat={Infinity}
             className="text-xl md:text-4xl text-purple-400"
-            cursor={true}
-          />
+            cursor={true}/>
 
           <p className="mt-4 text-gray-400 text-lg md:text-2xl max-w-xl">
             Estudante de Engenharia de Software com foco em desenvolvimento
@@ -60,15 +58,13 @@ function Hero() {
         </div>
 
       
-        <div className="flex justify-center md:justify-end">
+        <div className="flex">
           <img
             src="/images/developer.png"
             alt="Ilustração de Desenvolvedora"
             className="
-              max-w-[550px]
-              md:max-w-[650px]
-              lg:max-w-[750px]
-              xl:max-w-[850px]
+              w-full 
+              max-w-[900px]
               object-contain
               drop-shadow-[0_0_40px_rgba(168,85,247,0.35)]"/>
         </div>
